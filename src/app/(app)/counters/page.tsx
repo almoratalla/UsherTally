35ea50b734027c1be9b007ca4987082ef8657d2f
Alain Moratalla <realmoratalla@gmail.com>
2024-11-04 04:13:02 +0800

    fix: readded counterspanel in counters page
    Signed-off-by: Alain Moratalla <realmoratalla@gmail.com>

--- src/app/(app)/counters/page.tsx
+++ src/app/(app)/counters/page.tsx
@@ -1,13 +1,15 @@
 import Header from "@/app/components/Header";
+import CountersPanel from "@/app/components/panels/counters-panel";
 
 const isLoggedIn = true;
 
 const Counters = () => {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <Header />
+            <CountersPanel />
+        </div>
+    );
 };
 
 export default Counters;
