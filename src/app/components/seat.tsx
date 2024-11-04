193fa9cbe2fe811ef3e518d11f35853ef63a1c61
Alain Moratalla <realmoratalla@gmail.com>
2024-11-05 03:40:06 +0800

    style: update spacing for seat commit
    Signed-off-by: Alain Moratalla <realmoratalla@gmail.com>

--- src/app/components/seat.tsx
+++ src/app/components/seat.tsx
@@ -1,17 +1,17 @@
 import { cn } from "@/lib/utils";
 
 interface SeatProps {
-    isSelected: boolean;
-    onClick: () => void;
+  isSelected: boolean;
+  onClick: () => void;
 }
 
 export const Seat: React.FC<SeatProps> = ({ isSelected = true, onClick }) => (
-    <div
-        className={cn(
-            "bg-gray-800 h-6 w-8 m-1 rounded-t-lg cursor-pointer",
-            isSelected && "bg-green-700"
-        )}
-    ></div>
+  <div
+    className={cn(
+      "bg-gray-800 h-6 w-8 m-1 rounded-t-lg cursor-pointer",
+      isSelected && "bg-green-700",
+    )}
+  ></div>
 );
 
 export default Seat;
