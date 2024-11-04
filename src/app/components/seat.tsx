a5cc6cc3964a256de27adc202995214c656b6207
Alain Moratalla <realmoratalla@gmail.com>
2024-11-05 05:22:02 +0800

    fix: added seat-section planner poc
    Signed-off-by: Alain Moratalla <realmoratalla@gmail.com>

--- src/app/components/seat.tsx
+++ src/app/components/seat.tsx
@@ -1,17 +1,35 @@
 import { cn } from "@/lib/utils";
+import { PropsWithChildren } from "react";
 
 interface SeatProps {
-  isSelected: boolean;
-  onClick: () => void;
+    index: number;
+    isSelected: boolean;
+    onClick: () => void;
+    onMouseDown?: () => void;
+    onMouseOver?: () => void;
 }
 
-export const Seat: React.FC<SeatProps> = ({ isSelected = true, onClick }) => (
-  <div
-    className={cn(
-      "bg-gray-800 h-6 w-8 m-1 rounded-t-lg cursor-pointer",
-      isSelected && "bg-green-700",
-    )}
-  ></div>
+export const Seat: React.FC<PropsWithChildren & SeatProps> = ({
+    index,
+    isSelected = true,
+    onClick,
+    onMouseDown,
+    onMouseOver,
+}) => (
+    <div
+        key={`seat-${index}`}
+        onClick={onClick}
+        data-index={index}
+        className={cn(
+            "bg-gray-700 h-6 w-8 m-1 rounded-t-lg cursor-pointer flex flex-col items-center justify-center text-white text-xs",
+            isSelected && "bg-brand-selected border-green-700 border-2"
+        )}
+        onMouseDown={onMouseDown}
+        onMouseOver={onMouseOver}
+        style={{ userSelect: "none" }}
+    >
+        {(index + 1).toString()}
+    </div>
 );
 
 export default Seat;
