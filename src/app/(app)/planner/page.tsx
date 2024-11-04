f9f689c326ec47378aa954fed9d5cce03ceb655b
Alain Moratalla <realmoratalla@gmail.com>
2024-11-05 06:36:30 +0800

    fix: update planner page next page bug
    Signed-off-by: Alain Moratalla <realmoratalla@gmail.com>

--- src/app/(app)/planner/page.tsx
+++ src/app/(app)/planner/page.tsx
@@ -1,21 +1,18 @@
 "use client";
 import Header from "@/app/components/Header";
-import Seat from "@/app/components/seat";
 import SeatSection from "@/app/components/seat-section";
-import { cn } from "@/lib/utils";
-import { PropsWithChildren, useEffect, useState } from "react";
 
 // Seat Component
 
-export const Planner: React.FC<PropsWithChildren> = () => {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
-      <div className=" max-w-7xl mx-auto px-4 sm:px-6 2xl:px-0 py-12">
-        <SeatSection totalSeats={123} />
-      </div>
-    </div>
-  );
+const Planner = () => {
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <Header />
+            <div className=" max-w-7xl mx-auto px-4 sm:px-6 2xl:px-0 py-12">
+                <SeatSection totalSeats={123} />
+            </div>
+        </div>
+    );
 };
 
 export default Planner;
