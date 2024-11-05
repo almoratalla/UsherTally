e74ae1d268e619e190b4b23b25fc2b96b9893ad7
Alain Moratalla <realmoratalla@gmail.com>
2024-11-06 05:58:36 +0800

    fix: added planner-panel
    Signed-off-by: Alain Moratalla <realmoratalla@gmail.com>

--- src/app/(app)/planner/page.tsx
+++ src/app/(app)/planner/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 import Header from "@/app/components/Header";
+import PlannerPanel from "@/app/components/panels/planner-panel";
 import SeatSection from "@/app/components/seat-section";
 
 // Seat Component
 
 const Planner = () => {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <Header />
-      <div className=" max-w-7xl mx-auto px-4 sm:px-6 2xl:px-0 py-12">
-        <SeatSection totalSeats={123} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <Header />
+            <PlannerPanel />
+            <div className=" max-w-7xl mx-auto px-4 sm:px-6 2xl:px-0 py-12"></div>
+        </div>
+    );
 };
 
 export default Planner;
