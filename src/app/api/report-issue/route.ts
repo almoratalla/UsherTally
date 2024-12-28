4ac4ca750fc23d3b9f38aa3db6c2a4b2749545ec
Alain Moratalla <realmoratalla@gmail.com>
2024-12-29 05:08:48 +0800

    style: update formatting for the changes
    Signed-off-by: Alain Moratalla <realmoratalla@gmail.com>

--- src/app/api/report-issue/route.ts
+++ src/app/api/report-issue/route.ts
@@ -3,18 +3,18 @@ import { getFirestore } from "firebase-admin/firestore";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-    const data = await req.json();
+  const data = await req.json();
 
-    try {
-        await initAdmin();
-        const firestore = getFirestore();
-        const reports = firestore.collection("reports");
-        await reports.add(data);
-        return Response.json({ message: "Report submitted" }, { status: 200 });
-    } catch (error) {
-        return Response.json(
-            { message: "Failed to submit report", error },
-            { status: 500 }
-        );
-    }
+  try {
+    await initAdmin();
+    const firestore = getFirestore();
+    const reports = firestore.collection("reports");
+    await reports.add(data);
+    return Response.json({ message: "Report submitted" }, { status: 200 });
+  } catch (error) {
+    return Response.json(
+      { message: "Failed to submit report", error },
+      { status: 500 },
+    );
+  }
 }
